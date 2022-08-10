abcf0e933dead1c9cdf0e58f5d897c8b416db37a
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-08-10 10:17:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ RemoveBG_API = environ.get("RemoveBG_API", "")
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mkn_bots_updates')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Leya_updates')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
