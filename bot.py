4c5cef1d62cabbe1ba4f66c4699849ec6272b522
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-08-10 10:07:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("🚀Restarting..PROFESSOR-BOT💥")
+        logging.info("🚀Restarting..LEYA-BOT💥")
 
     
 app = Bot()
